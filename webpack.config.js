c51bb2103e7560f58b31219bb0ec4d7870ab255e
Zoupse <charlotte.matusac@gmail.com>
2020-05-05 08:35:31 +0200

    Install Webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: true,
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+    }
+};
