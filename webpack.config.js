44ca666bf45907975100aa165fd45d448af2de30
Zoupse <charlotte.matusac@gmail.com>
2020-05-05 08:52:26 +0200

    Webpack - CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contentHash].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -20,6 +21,7 @@ module.exports = {
         ],
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: true,
